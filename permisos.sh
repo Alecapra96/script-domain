87fb30ac2a075944ab6187e68381c4e4013638af
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-11-09 13:58:27 -0300

    Update permisos.sh

--- permisos.sh
+++ permisos.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 sudo apt -y install dialog
 # creamos la funcion fundialog 
+fundialog=${fundialog=dialog}
 
-
+echo "Dependencias para el  dominio"
+sudo apt -y install sssd-ad sssd-tools realmd adcli sed 
+sudo apt-get -y install realmd packagekit
 
 #_______________
 dialog --infobox "Bienvenido al script para administrar los permisos del dominio en la maquina!" 0 0 ; sleep 3
@@ -14,7 +17,7 @@ trap "rm -f $tempfile" 0 1 2 5 15
 
 $DIALOG --backtitle "Creado por : linkedin.com/in/alejandro-capra" \
 	--title "PERMISOS DEL DOMINIO" --clear \
-        --radiolist "Selecciona el permiso que deseas" 20 61 5 \
+        --radiolist "Selecciona el permiso que deseas" 40 125 10 \
         "1"    "Denegar login a todos los usuarios y permitir login de un usuario en particular" off \
         "2"    "Denegar login a todos los usuarios y permitir login de un grupo en particular" off \
         "3"  "Permitir login a todos los usuarios del realm/Dominio" off \
@@ -28,7 +31,53 @@ retval=$?
 choice=`cat $tempfile`
 case $retval in
   0)
-    echo "'$choice' is your favorite singer";;
+    echo "Elegiste la opcion '$choice'"
+    nombreDominio=`$fundialog --stdout --title "nombre" --inputbox "Escribe el nuevo nombre del dominio mas el .local :" 0 0`
+
+
+    case $choice in
+     1)echo "Denegar login a todos los usuarios y permitir login de un usuario en particular"
+         nombreUsuario=`$fundialog --stdout --title "nombre" --inputbox "Escribe el nuevo nombre del usuario:" 0 0`
+
+     sudo realm deny -vR $nombreDominio -a
+     sudo realm permit -vR $nombreDominio $nombreUsuario
+
+     ;;
+     2)echo "Denegar login a todos los usuarios y permitir login de un grupo en particular"
+         nombreGrupo=`$fundialog --stdout --title "nombre" --inputbox "Escribe el nuevo nombre del grupo mas el @dominio.local" 0 0`
+
+     sudo realm deny -vR $nombreDominio -a
+     sudo realm permit -vR $nombreDominio -g  $nombreGrupo
+
+     ;;
+     3)echo "Permitir login a todos los usuarios del realm/Dominio"
+     sudo realm permit -vR $nombreDominio -a
+
+     ;;
+     4)echo "Permitir login a un usuario particular del realm/Dominio"
+         nombreUsuario=`$fundialog --stdout --title "nombre" --inputbox "Escribe el nuevo nombre del usuario:" 0 0`
+
+     sudo realm permit -vR $nombreDominio $nombreUsuario
+
+     ;;
+     5)echo "Denegar login a todos los usuarios del realm/Dominio"
+     sudo realm deny -vR $nombreDominio -a
+
+     ;;
+     6)echo "Permitir login a los miembros de un Grupo"
+         nombreGrupo=`$fundialog --stdout --title "nombre" --inputbox "Escribe el nuevo nombre del grupo mas el @dominio.local" 0 0`
+
+     sudo realm permit -vR $nombreDominio -g $nombreGrupo
+
+     ;;
+     7)echo "Denegar login a los miembros de un Grupo ya agregado"
+         nombreGrupo=`$fundialog --stdout --title "nombre" --inputbox "Escribe el nuevo nombre del grupo mas el @dominio.local" 0 0`
+
+     sudo realm permit -vR $nombreDominio -x $nombreGrupo
+
+    ;;
+    esac 
+    ;;
   1)
     echo "Cancel pressed.";;
   255)
